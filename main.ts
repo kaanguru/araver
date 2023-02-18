f7101e5d83ff9f07b0fef0cdf9e5574c9256cfdf
kaanguru <kaanguru@mail.ru>
2023-02-18 16:25:53 +0300

    update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ const breakTitle = chrome.i18n.getMessage("breakTitle");
 const breakContent = (s: string): string => chrome.i18n.getMessage("breakContent", s);
 const reminderTitle = chrome.i18n.getMessage("reminderTitle");
 const reminderContent = chrome.i18n.getMessage("reminderContent");
-const pauseIconUrls = ["icons/icons8_pause_64px.png", "icons8_Body_Positive_Female_64px_1.png", "jumping-man.png"];
+const pauseIconUrls = ["icons/icons8_pause_64px.png", "icons/icons8_Body_Positive_Female_64px_1.png", "icons/jumping-man.png"];
 
 class PauseScreen {
   settings: PauseScreenSettings = null;
@@ -27,18 +27,22 @@ class PauseScreen {
     let now: Date = new Date();
     const timeForLogging = pauseReminder.getTimeForLogging();
     console.log(`${timeForLogging}: state change, new state ${newState}`);
+
+    // Calculate time difference
+    let diff: number = now.getTime() - pauseReminder.lastStateChangeTimestamp;
+    let minutes: number = Math.round(diff / (1000 * 60));
+
+    // Change behaviour based on new state
     if (newState != "active") {
       pauseReminder.timeWithoutBreak += Math.round((now.getTime() - pauseReminder.lastStateChangeTimestamp) / (60 * 1000));
       chrome.alarms.clearAll();
     } else if (newState == "active") {
-      let diff: number = now.getTime() - pauseReminder.lastStateChangeTimestamp;
-      let minutes: number = Math.round(diff / (1000 * 60));
       if (minutes >= pauseReminder.settings.breakDuration - 1) {
-        // because 1 minute passes bewfore we go in idle srare
-        console.log(`${timeForLogging}: away for  ${(diff / (1000 * 60)).toString()}m reseting break timer`);
+        // because 1 minute passes before we go in idle state
+        console.log(`${timeForLogging}: away for  ${(diff / (1000 * 60)).toString()} min reseting break timer`);
         pauseReminder.timeWithoutBreak = 0;
       } else {
-        console.log(`${timeForLogging}:: short break for  ${minutes.toString()} " minutes`);
+        console.log(`${timeForLogging} short break for  ${minutes.toString()}  minutes`);
         pauseReminder.timeWithoutBreak -= minutes;
       }
       chrome.alarms.create("reminder_alarm", { periodInMinutes: pauseReminder.settings.remindInterval });
@@ -50,13 +54,21 @@ class PauseScreen {
     pauseReminder.timeWithoutBreak += pauseReminder.settings.remindInterval;
     if (pauseReminder.timeWithoutBreak >= pauseReminder.settings.breakInterval) {
       console.log(`${timeForLogging}: Long Break, without break since : ${pauseReminder.timeWithoutBreak.toString()} minutes`);
-      chrome.notifications.create("pause", { type: "basic", title: breakTitle, iconUrl: "icons/icons8_stretching_64px.png", message: breakContent(pauseReminder.settings.breakDuration.toString()) });
+      chrome.notifications.create("pause", {
+        type: "basic",
+        title: breakTitle,
+        iconUrl: "icons/icons8_stretching_64px.png",
+        message: breakContent(pauseReminder.settings.breakDuration.toString()),
+      });
     } else {
       console.log(`${timeForLogging}:  Short Pause, without break since  ${pauseReminder.timeWithoutBreak.toString()} minutes`);
-      let randomString = pauseIconUrls[Math.floor(Math.random() * pauseIconUrls.length)];
-
-      console.log(randomString);
-      chrome.notifications.create("pause", { type: "basic", title: reminderTitle, iconUrl: pauseIconUrls[0], message: reminderContent });
+      let randomIcon = pauseIconUrls[Math.floor(Math.random() * pauseIconUrls.length)];
+      chrome.notifications.create("pause", {
+        type: "basic",
+        title: reminderTitle,
+        iconUrl: randomIcon,
+        message: reminderContent,
+      });
     }
   }
   loadSettings(): void {
