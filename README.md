863dc561f1af31e9fba4113f4dff5d52103e2e37
kaanguru <kaanguru@mail.ru>
2023-02-18 16:01:09 +0300

    refactor

--- README.md
+++ README.md
@@ -9,10 +9,52 @@ WebExtension for Firefox. Set reminders to take...
 
 The addon is available on Mozialla addons page <https://addons.mozilla.org/en-US/firefox/addon/blink-reminder/>
 
-### Todo
+### PauseScreen Class Documentation
+The 
+PauseScreen
+ class is responsible for managing the settings, time without break, and last state change timestamp of the application. It also contains methods to initialize the application, detect state changes, handle alarm firings, and load settings.
 
-1. i18n <https://github.com/mdn/webextensions-examples/tree/main/notify-link-clicks-i18n>
-1. Two different icons
+Properties
+settings
+ - A 
+PauseScreenSettings
+ object that stores the application settings
+timeWithoutBreak
+ - An integer that stores the amount of time without a break
+lastStateChangeTimestamp
+ - An integer that stores the last time the state of the application changed
+Methods
+init()
+This method is responsible for initializing the application. It sets the detection interval, adds listeners for settings changes, state changes, and alarm firings. It also creates an alarm with the 
+remindInterval
+ from the settings.
+
+onStateChanged()
+This method is responsible for handling state changes. It calculates the amount of time spent in the new state and either resets the break timer or subtracts the amount of time spent in the new state from the break timer.
+
+onAlarmFired()
+This method is responsible for handling alarm firings. It adds the 
+remindInterval
+ from the settings to the 
+timeWithoutBreak
+ and then creates a notification based on the amount of time without a break.
+
+loadSettings()
+This method is responsible for loading the settings from storage. It sets the 
+settings
+ property with the settings from storage and then calls the 
+init()
+ method.
+
+onSettingsChanged()
+This method is responsible for handling settings changes. It clears all alarms, updates the 
+settings
+ property, and then creates an alarm with the updated 
+remindInterval
+.
+
+getTimeForLogging()
+This method is responsible for getting the current time in a format suitable for logging.
 
 ---
 
--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 /// <reference path="options.ts" />
 
 const breakTitle = chrome.i18n.getMessage("breakTitle");
-function breakContent(s: string): string {
-  return chrome.i18n.getMessage("breakContent", s);
-}
+const breakContent = (s: string): string => chrome.i18n.getMessage("breakContent", s);
 const reminderTitle = chrome.i18n.getMessage("reminderTitle");
 const reminderContent = chrome.i18n.getMessage("reminderContent");
+const pauseIconUrls = ["icons/icons8_pause_64px.png", "icons8_Body_Positive_Female_64px_1.png", "jumping-man.png"];
 
 class PauseScreen {
   settings: PauseScreenSettings = null;
@@ -26,7 +25,8 @@ class PauseScreen {
   }
   onStateChanged(newState: string): void {
     let now: Date = new Date();
-    console.log(pauseReminder.getTimeForLogging().concat(": state change, new state ", newState));
+    const timeForLogging = pauseReminder.getTimeForLogging();
+    console.log(`${timeForLogging}: state change, new state ${newState}`);
     if (newState != "active") {
       pauseReminder.timeWithoutBreak += Math.round((now.getTime() - pauseReminder.lastStateChangeTimestamp) / (60 * 1000));
       chrome.alarms.clearAll();
@@ -35,10 +35,10 @@ class PauseScreen {
       let minutes: number = Math.round(diff / (1000 * 60));
       if (minutes >= pauseReminder.settings.breakDuration - 1) {
         // because 1 minute passes bewfore we go in idle srare
-        console.log(pauseReminder.getTimeForLogging().concat(": away for ", (diff / (1000 * 60)).toString(), "m reseting break timer"));
+        console.log(`${timeForLogging}: away for  ${(diff / (1000 * 60)).toString()}m reseting break timer`);
         pauseReminder.timeWithoutBreak = 0;
       } else {
-        console.log(pauseReminder.getTimeForLogging().concat(": short break for ", minutes.toString(), " minutes"));
+        console.log(`${timeForLogging}:: short break for  ${minutes.toString()} " minutes`);
         pauseReminder.timeWithoutBreak -= minutes;
       }
       chrome.alarms.create("reminder_alarm", { periodInMinutes: pauseReminder.settings.remindInterval });
@@ -46,31 +46,34 @@ class PauseScreen {
     pauseReminder.lastStateChangeTimestamp = now.getTime();
   }
   onAlarmFired(alarm: chrome.alarms.Alarm): void {
+    const timeForLogging = pauseReminder.getTimeForLogging();
     pauseReminder.timeWithoutBreak += pauseReminder.settings.remindInterval;
     if (pauseReminder.timeWithoutBreak >= pauseReminder.settings.breakInterval) {
-      console.log(pauseReminder.getTimeForLogging().concat(": Uzun Ara, without break since ", pauseReminder.timeWithoutBreak.toString(), " minutes"));
-      chrome.notifications.create("ara_ver", { type: "basic", title: breakTitle, iconUrl: "icons/icons8_stretching_64px.png", message: breakContent(pauseReminder.settings.breakDuration.toString()) });
+      console.log(`${timeForLogging}: Long Break, without break since : ${pauseReminder.timeWithoutBreak.toString()} minutes`);
+      chrome.notifications.create("pause", { type: "basic", title: breakTitle, iconUrl: "icons/icons8_stretching_64px.png", message: breakContent(pauseReminder.settings.breakDuration.toString()) });
     } else {
-      console.log(pauseReminder.getTimeForLogging().concat(": Kısa Ara, without break since ", pauseReminder.timeWithoutBreak.toString(), " minutes"));
-      chrome.notifications.create("ara_ver", { type: "basic", title: reminderTitle, iconUrl: "icons/icons8_pause_64px.png", message: reminderContent });
+      console.log(`${timeForLogging}:  Short Pause, without break since  ${pauseReminder.timeWithoutBreak.toString()} minutes`);
+      let randomString = pauseIconUrls[Math.floor(Math.random() * pauseIconUrls.length)];
+
+      console.log(randomString);
+      chrome.notifications.create("pause", { type: "basic", title: reminderTitle, iconUrl: pauseIconUrls[0], message: reminderContent });
     }
   }
   loadSettings(): void {
-    chrome.storage.local.get(
-      {
-        remindInterval: 20,
-        breakInterval: 60,
-        breakDuration: 10,
-      },
-      (s: PauseScreenSettings) => {
-        if (!chrome.runtime.lastError) {
-          pauseReminder.settings = s;
-          pauseReminder.init(s);
-        } else {
-          console.error("Error loading settings ".concat(chrome.runtime.lastError.message));
-        }
+    const settings = {
+      remindInterval: 20,
+      breakInterval: 60,
+      breakDuration: 10,
+    };
+
+    chrome.storage.local.get(settings, (s: PauseScreenSettings) => {
+      if (!chrome.runtime.lastError) {
+        pauseReminder.settings = s;
+        pauseReminder.init(s);
+      } else {
+        console.error(`Error loading settings ${chrome.runtime.lastError.message}`);
       }
-    );
+    });
   }
   onSettingsChanged(change: { [key: string]: chrome.storage.StorageChange }, area: string): void {
     chrome.alarms.clearAll();
@@ -80,9 +83,9 @@ class PauseScreen {
     chrome.alarms.create("reminder_alarm", { periodInMinutes: pauseReminder.settings.remindInterval });
   }
   getTimeForLogging(): string {
-    let now: Date = new Date();
-    let hours: string = now.getHours() > 9 ? now.getHours().toString() : "0" + now.getHours().toString();
-    let minutes: string = now.getMinutes() > 9 ? now.getMinutes().toString() : "0" + now.getMinutes().toString();
+    const now: Date = new Date();
+    const hours: string = now.getHours() > 9 ? now.getHours().toString() : "0" + now.getHours().toString();
+    const minutes: string = now.getMinutes() > 9 ? now.getMinutes().toString() : "0" + now.getMinutes().toString();
     return "[".concat(hours, ":", minutes, "]");
   }
 }
